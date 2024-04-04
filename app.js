11c29249d7efac93b9cf74c1568f0f923538bd31
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-04 13:10:27 -0400

    init finished

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 /*-------------------------------- Constants --------------------------------*/
+const numbers = document.querySelectorAll(".number");
 
 /*-------------------------------- Variables --------------------------------*/
 
+
 /*------------------------ Cached Element References ------------------------*/
 
+
 /*----------------------------- Event Listeners -----------------------------*/
+numbers.forEach(number => {
+  number.addEventListener("click", (event) => {
+    // This log is for testing purposes to verify we're getting the correct value
+    console.log(event.target.innerText);
+    // Future logic to capture the button's value goes here...
+  });
+});
 
 /*-------------------------------- Functions --------------------------------*/
+

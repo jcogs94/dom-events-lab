5765ff4756194d2d31ec048a007e23d9805d7133
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-04 16:01:56 -0400

    clear function

--- app.js
+++ app.js
@@ -9,25 +9,15 @@ let num1 = 0;
 let num2 = 0;
 let displayEntry2 = false;
 let operatorPressed = false;
-let operatorType = '';
+let operatorType = emptyEntry;
 
 /*------------------------ Cached Element References ------------------------*/
 const numbersElement = document.querySelectorAll(".number");
-const displayElement = document.querySelector('.display')
-
-/*----------------------------- Event Listeners -----------------------------*/
-numbersElement.forEach(number => {
-  number.addEventListener("click", (event) => {
-    // // This log is for testing purposes to verify we're getting the correct value
-    // console.log(event.target.innerText, typeof(event.target.innerText));
-
-    // Future logic to capture the button's value goes here...
-    numPressed(event.target.innerText);
-  });
-});
+const displayElement = document.querySelector('.display');
+const clearElement = document.querySelector('#clear');
 
 /*-------------------------------- Functions --------------------------------*/
-const numPressed = (inputNum) => {
+const numPressedHandler = (inputNum) => {
     if (!displayEntry2 && entry1.length < 18) {
         if (entry1.length === 1 && entry1 === noEntry)
             entry1 = inputNum;
@@ -36,4 +26,34 @@ const numPressed = (inputNum) => {
         
         displayElement.innerText = entry1;
     }
+    else if (displayEntry2 && entry2.length < 18) {
+
+    }
 }
+
+const clearHandler = () => {
+    entry1 = noEntry;
+    entry2 = emptyEntry;
+    num1 = 0;
+    num2 = 0;
+    
+    if (operatorPressed) {
+        operatorPressed = false;
+        operatorType = emptyEntry;
+    }
+
+    displayElement.innerText = entry1;
+}
+
+/*----------------------------- Event Listeners -----------------------------*/
+numbersElement.forEach(number => {
+  number.addEventListener("click", (event) => {
+    // // This log is for testing purposes to verify we're getting the correct value
+    // console.log(event.target.innerText, typeof(event.target.innerText));
+
+    // Future logic to capture the button's value goes here...
+    numPressedHandler(event.target.innerText);
+  });
+});
+
+clearElement.addEventListener('click', clearHandler);

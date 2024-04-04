cc49c2f30461a23a1b8cc893389c2b69a2c50746
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-04 15:48:17 -0400

    display numbers from start

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 /*-------------------------------- Constants --------------------------------*/
-const numbers = document.querySelectorAll(".number");
+const noEntry = '0';
+const emptyEntry = '';
 
 /*-------------------------------- Variables --------------------------------*/
-
+let entry1 = noEntry;
+let entry2 = emptyEntry;
+let num1 = 0;
+let num2 = 0;
+let displayEntry2 = false;
+let operatorPressed = false;
+let operatorType = '';
 
 /*------------------------ Cached Element References ------------------------*/
-
+const numbersElement = document.querySelectorAll(".number");
+const displayElement = document.querySelector('.display')
 
 /*----------------------------- Event Listeners -----------------------------*/
-numbers.forEach(number => {
+numbersElement.forEach(number => {
   number.addEventListener("click", (event) => {
     // // This log is for testing purposes to verify we're getting the correct value
-    // console.log(event.target.innerText);
+    // console.log(event.target.innerText, typeof(event.target.innerText));
 
     // Future logic to capture the button's value goes here...
-    
+    numPressed(event.target.innerText);
   });
 });
 
 /*-------------------------------- Functions --------------------------------*/
-
+const numPressed = (inputNum) => {
+    if (!displayEntry2 && entry1.length < 18) {
+        if (entry1.length === 1 && entry1 === noEntry)
+            entry1 = inputNum;
+        else
+            entry1 += inputNum;
+        
+        displayElement.innerText = entry1;
+    }
+}
